e5184232c6d1e5d23bc18680079e18bb63aaa15c
n-nizar <167634786+n-nizar@users.noreply.github.com>
2024-05-30 17:17:08 -0400

    Updated dbt_audit macros

--- macros/dbt_audit/dbt_audit_start_exec.sql
+++
@@ -1,6 +0,0 @@
-{% macro dbt_audit_start_exec(invocation_id, execution_type, model) %}
-
-INSERT INTO {{ var('dbt_audit_database') }}.{{ var('dbt_audit_schema') }}.{{ var('dbt_audit_table') }} (invocation_id, execution_type, model, status, start_time)
-VALUES ('{{ invocation_id }}', '{{ execution_type }}', '{{ model }}', 'Started Execution', CURRENT_TIMESTAMP())
-
-{% endmacro %}
