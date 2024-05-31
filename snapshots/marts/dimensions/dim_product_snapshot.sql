9df9af87a095485ed955b053df3bad747e712dbf
n-nizar <167634786+n-nizar@users.noreply.github.com>
2024-05-31 18:23:27 -0400

    Updated dim_product_snapshot

--- snapshots/marts/dimensions/dim_product_snapshot.sql
+++ snapshots/marts/dimensions/dim_product_snapshot.sql
@@ -6,27 +6,13 @@
     "check_cols": ["ProductSK"]
 }) }}
 
-WITH source_data AS(
-    SELECT *
-    FROM {{ ref('sales_staging') }}
-),
-
-prep AS (
-    SELECT DISTINCT 
-        {{ dbt_utils.generate_surrogate_key(['ProductID', 'Product', 'Category', 'Segment', 'UnitCost', 'UnitPrice']) }}
-                                                                        AS ProductSK,
-        ProductID                                                       AS ProductID,
-        Product                                                         AS Product,
-        Category                                                        AS Category,
-        Segment                                                         AS Segment,
-        UnitCost                                                        AS UnitCost,
-        UnitPrice                                                       AS UnitPrice
-    FROM source_data
-)
-
-
-SELECT *,
-        CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP)::TIMESTAMP_NTZ       AS UpdatedTS
-FROM prep src
+SELECT
+    {{
+          dbt_utils.star(
+            from=ref('dim_product'),
+            except=['UpdatedTS']
+            )
+      }}
+    FROM {{ ref('dim_product') }}
 
 {% endsnapshot %}
